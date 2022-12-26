a2c9bec5fa7b8f9d172dcd841b8f69c0f438cc38
rlbandaraherath <84961744+rlbandaraherath@users.noreply.github.com>
2022-12-26 11:13:13 +0530

    Add files via upload

---
+++ src/main/java/com/mycompany/mssgroupproject/MSSGroupProject.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mssgroupproject;
+
+/**
+ *
+ * @author Roshan
+ */
+public class MSSGroupProject {
+
+    public static void main(String[] args) {
+        MainLogin mn = new MainLogin();
+        mn.setVisible(true);
+    }
+}
